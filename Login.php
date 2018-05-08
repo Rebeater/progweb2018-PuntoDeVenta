6aadaaebe5f757dd323e16455d04733012729c1d
Eber Reta <eber_bred@hotmail.com>
2018-05-07 19:15:49 -0600

    Entrega Login + crud (No funca completamente)

---
+++ Login.php
@@ -0,0 +1,60 @@
+<!Doctype html>
+<html>
+<meta charset="utf-8">
+<meta http-equiv="cache-control" content="no-cache" />
+<head>
+	<link rel="stylesheet" href="css/bootstrap.css">
+    <script src="js/bootstrap.js"></script>
+
+	<h1 class="bg-secondary text-white" style = "text-align: center;">Login</h1>
+</head>
+<body>
+<div class = "row">
+
+	<div class = "col">
+		
+	</div>
+	
+	<div class = "col">
+		
+	</div>
+	
+	<div class = "col">
+		
+	</div>
+	
+
+	<div class = "col">
+	
+		<form method="post" action="<?php echo htmlspecialchars('Accion.php');?>">
+			Usuario:
+			<br>
+			<input type = "text" name = "correo" placeholder = "Correo" maxlength="30">
+			<br>
+			Contraseña:
+			<br>
+			<input type = "password" name = "contraseña" placeholder = "contraseña" maxlength = "10">
+			<br>
+			<input type = "submit" value = "Ingresar">
+			
+		</form>
+	</div>
+	
+	<div class = "col">
+		
+	</div>
+
+	<div class = "col">
+		
+	</div>
+	
+	<div class = "col">
+		
+	</div>
+	
+</div>
+
+
+	
+</body>
+</html>
